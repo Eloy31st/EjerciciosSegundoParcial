0c1a92f45684ef448ea76ff06924c53cee0a768b
Eloy Sánchez Tamayo <esanctam@myuax.com>
2023-04-13 10:27:41 +0200

    Apartado b Ejercicio 4

---
+++ src/main/java/Ship.java
@@ -0,0 +1,98 @@
+import java.awt.*;
+
+public class Ship {
+    private CardinalPoints direccion;
+    private int tamano;
+    private int numTocado;
+    private Point puntoInicio;
+    private Point puntoFinal;
+
+    public Ship(int tamano, Point puntoInicio, Point puntoFinal) {
+        this.tamano = tamano;
+        this.puntoInicio = puntoInicio;
+        this.numTocado = 0;
+        if (puntoInicio.getX() == puntoFinal.getX()) {
+            if (puntoInicio.getY() < puntoFinal.getY()) {
+                this.direccion = CardinalPoints.SOUTH;
+            } else {
+                this.direccion = CardinalPoints.NORTH;
+            }
+        } else if (puntoInicio.getY() == puntoFinal.getY()) {
+            if (puntoInicio.getX() < puntoFinal.getX()) {
+                this.direccion = CardinalPoints.EAST;
+            } else {
+                this.direccion = CardinalPoints.WEST;
+            }
+        } else {
+            throw new IllegalArgumentException("Los puntos no forman una linea recta");
+        }
+
+    }
+
+    public CardinalPoints getDireccion() {
+        return direccion;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public int getNumTocado() {
+        return numTocado;
+    }
+
+    public Point getPuntoInicio() {
+        return puntoInicio;
+    }
+
+    public Point getPuntoFinal() {
+        return puntoFinal;
+    }
+
+    public void setPuntoInicio(Point puntoInicio) {
+        this.puntoInicio = puntoInicio;
+    }
+
+    public void setPuntoFinal(Point puntoFinal) {
+        this.puntoFinal = puntoFinal;
+    }
+
+    public boolean is_Sunk() {
+        return this.numTocado == this.tamano;
+    }
+
+    public boolean get_Shot(Point shot) {
+        if (direccion == CardinalPoints.NORTH) {
+            if (shot.getX() != this.puntoInicio.getX()) {
+                return false;
+            } else if (shot.getY() <= this.puntoInicio.getY() && shot.getY() >= this.puntoFinal.getY()) {
+                numTocado++;
+                return true;
+            }
+        } else if (direccion == CardinalPoints.SOUTH) {
+            if (shot.getX() != this.puntoInicio.getX()) {
+                return false;
+            } else if (shot.getY() >= this.puntoInicio.getY() && shot.getY() <= this.puntoFinal.getY()) {
+                numTocado++;
+                return true;
+
+            }
+        } else if (direccion == CardinalPoints.EAST) {
+            if (shot.getY() != this.puntoInicio.getY()) {
+                return false;
+            } else if (shot.getX() >= this.puntoInicio.getX() && shot.getX() <= this.puntoFinal.getX()) {
+                numTocado++;
+                return true;
+            }
+        } else if (direccion == CardinalPoints.WEST) {
+            if (shot.getY() != this.puntoInicio.getY()) {
+                return false;
+            } else if (shot.getX() <= this.puntoInicio.getX() && shot.getX() >= this.puntoFinal.getX()) {
+                numTocado++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
